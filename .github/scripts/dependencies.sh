3e7fa2cfcec17095a753a54c9c5b10acadb43648
Vlad <110987133+vladiAN@users.noreply.github.com>
2023-06-15 14:36:34 +0300

    Update dependencies.sh

--- .github/scripts/dependencies.sh
+++ .github/scripts/dependencies.sh
@@ -2,14 +2,6 @@
 
 set -eo pipefail
 
-steps:
-  - name: Set execute permission
-    run: chmod +x ./.github/scripts/dependencies.sh
-
-  - name: Run dependencies.sh
-    run: ./.github/scripts/dependencies.sh
-
-
     if [ -e "$NAME.xcworkspace" ]
 then
     xcodebuild -workspace "$NAME".xcworkspace -scheme "$NAME" -resolvePackageDependencies
